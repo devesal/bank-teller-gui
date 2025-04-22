3f6a090a878c8d251cd23dc921c8b244e1d93af9
Ricky Marc Ramos <rickydevramos@gmail.com>
2025-04-22 20:00:30 +0800

    Add remaining customer form pages

--- src/main/java/edu/slu/cs/controller/CustomerFormController.java
+++ src/main/java/edu/slu/cs/controller/CustomerFormController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
 import edu.slu.cs.util.FXMLLoaderUtil;
 
@@ -22,15 +23,14 @@ public class CustomerFormController {
 
     @FXML
     private void handleBack(ActionEvent event) {
-        int nextPage = currentPage - 1;
+        int pageIndex = currentPage - 1;
 
         Parent root = null;
-
         try {
-            if (nextPage < 1) {
+            if (pageIndex < 1) {
                 root = FXMLLoaderUtil.load("/view/HomeView.fxml");
             } else {
-                root = loadNextPage(nextPage);
+                root = loadNextPage(pageIndex);
             }
         } catch (IOException e) {
             System.err.println("Failed to load " + "Step" + currentPage + " fxml");
@@ -38,11 +38,10 @@ public class CustomerFormController {
             System.err.println("Could not find " + "Step" + currentPage + " fxml. Check the path.");
         }
 
-        currentPage--;
+        if (currentPage > 1) {currentPage--;}
 
-        Stage stage = (Stage) nextButton.getScene().getWindow();
+        Stage stage = (Stage) backButton.getScene().getWindow();
         Scene scene = new Scene(root, 1240, 700);
-
         stage.setScene(scene);
         stage.show();
     }
@@ -69,8 +68,22 @@ public class CustomerFormController {
         stage.show();
     }
 
+    @FXML
+    private void handleAccountSelection(ActionEvent event) {
+        ToggleButton btn = (ToggleButton) event.getSource();
+        if (btn.isSelected()) {
+            btn.setStyle("-fx-background-color: #1F883D; -fx-text-fill: white");
+        } else {
+            btn.setStyle("-fx-background-color: #D1D9E0; -fx-text-fill: black");
+        }
+    }
+
     private Parent loadNextPage(int nextPage) throws IOException, NullPointerException {
         String fxmlPath = String.format("/view/customerform/Step%d.fxml", nextPage);
         return FXMLLoaderUtil.load(fxmlPath);
+    }
+
+    public void handleCreate(ActionEvent actionEvent) {
+        // TODO
     }
 }
