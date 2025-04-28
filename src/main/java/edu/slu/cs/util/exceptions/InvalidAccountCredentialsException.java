5b239e3e28abb4a77669893fff939f23cfbdff8b
spinooooo <clendrickarellano@gmail.com>
2025-04-29 06:55:01 +0800

    Renamed the InvalidAccountCredentialsException Class to InvalidCredentialsException

--- src/main/java/edu/slu/cs/util/exceptions/InvalidAccountCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package edu.slu.cs.util.exceptions;
-
-public class InvalidAccountCredentialsException extends Exception {
-    public InvalidAccountCredentialsException (String message){
-        super(message);
-    }
-}
---
+++ src/main/java/edu/slu/cs/util/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package edu.slu.cs.util.exceptions;
+
+public class InvalidCredentialsException extends Exception {
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
+}
