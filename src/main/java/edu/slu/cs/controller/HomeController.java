ec3e5b03387b49dd915598c65ee704651435bf93
AEZED05 <john2005217@gmail.com>
2025-04-28 22:34:31 +0800

    Started working on searching

--- src/main/java/edu/slu/cs/controller/HomeController.java
+++ src/main/java/edu/slu/cs/controller/HomeController.java
@@ -5,14 +5,23 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import edu.slu.cs.util.FXMLLoaderUtil;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.slu.cs.implementation.BankServices;
 
 public class HomeController {
-
+    BankServices customer = new BankServices();
     @FXML
     Button addCustomerButton;
+    @FXML
+    TextField searchField;
+
 
     @FXML
     private void handleAddCustomer(ActionEvent event) {
@@ -29,5 +38,24 @@ public class HomeController {
         } catch (NullPointerException e) {
             System.err.println("Could not find Step1.fxml. Check the path.");
         }
+    }
+
+    @FXML
+    public void searchButton(ActionEvent actionEvent) {
+        searchField.setOnKeyReleased(e -> {
+            if(e.getCode() == KeyCode.ENTER){
+                String searchText = searchField.getText().trim();
+
+
+                if(searchText.matches("\\d{10}")){
+                    int accountNum = Integer.parseInt(searchText);
+                    customer.searchAccountByNumber(accountNum);
+                }
+
+                if(searchText.matches("[a-zA-Z\\s]+")){
+                    customer.searchAccountsByName(searchText);
+                }
+            }
+        });
     }
 }
