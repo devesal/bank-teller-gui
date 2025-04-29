ee2ccf409f269d243086f739e33876f8a59f38fb
Ron Miguel Ong <bruhitzcalibre@gmail.com>
2025-04-29 22:17:15 +0800

    Add the FileIO Reference Class for data storage. I will send the .dat file with pre-existing accounts soon

---
+++ src/main/java/edu/slu/cs/model/FileIO.java
@@ -0,0 +1,35 @@
+package edu.slu.cs.model;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileIO {
+
+    private static final String ACCOUNTS_DATA_FILE = "all_accounts.dat";
+
+
+    public static void saveAllAccounts(ArrayList<BankAccount> accounts) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(
+                new FileOutputStream(ACCOUNTS_DATA_FILE))) {
+            oos.writeObject(accounts);
+        } catch (IOException e) {
+            System.out.println("Error saving accounts: " + e.getMessage());
+        }
+    }
+
+
+    public static ArrayList<BankAccount> loadAllAccounts() {
+        File file = new File(ACCOUNTS_DATA_FILE);
+        if (!file.exists()) {
+            return new ArrayList<>(); // Return empty list if file doesn't exist
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(
+                new FileInputStream(ACCOUNTS_DATA_FILE))) {
+            return (ArrayList<BankAccount>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading accounts: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+}
