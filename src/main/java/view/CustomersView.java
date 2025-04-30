c38c97071ad8b19cd64f1ee09d62274d3baadbfe
AEZED05 <john2005217@gmail.com>
2025-04-30 17:54:18 +0800

    Implemented searching in Customer View

--- src/main/java/view/CustomersView.java
+++ src/main/java/view/CustomersView.java
@@ -3,21 +3,38 @@ package view;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.List;
+import model.Customer;
+import model.BankAccount;
 
-public class  CustomersView extends JPanel {
-
-    JTable table;
+public class CustomersView extends JPanel {
+    private JTable table;
 
     public CustomersView() {
         setLayout(new BorderLayout());
 
-        String[] cols = {"Account No", "Name", "Status"};
-        DefaultTableModel model = new DefaultTableModel(cols, 0);
+        String[] columns = {"Account No", "Name", "Status"};
+        DefaultTableModel model = new DefaultTableModel(columns, 0);
         table = new JTable(model);
         JScrollPane scroll = new JScrollPane(table);
-        scroll.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
         add(scroll, BorderLayout.CENTER);
     }
 
-    public JTable getTable() { return table; }
-}
+    public JTable getTable() {
+        return table;
+    }
+
+    public void updateTableWithSearchResults(List<BankAccount> matchingAccounts) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.setRowCount(0); // Clear existing rows
+
+
+        for (BankAccount account : matchingAccounts) {
+            model.addRow(new Object[]{
+                    account.getAccountNo(),                       // Account No
+                    account.getFirstName() + " " + account.getLastName(), // Account Holder Name
+                    account.getStatus()                          // Account Status
+            });
+            }
+        }
+    }
