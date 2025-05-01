1de002fa70c8f44fe422c269c6541d6aeebd0610
spinooooo <clendrickarellano@gmail.com>
2025-05-01 13:15:08 +0800

    Added the TransactionLogController class for transaction logging

---
+++ src/main/java/controller/TransactionLogController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import util.TransactionLogger;
+import view.MainView;
+import view.TransactionHistoryView;
+
+public class TransactionLogController {
+    private TransactionHistoryView tView;
+    private MainView mView;
+    private TransactionLogger transactionLog;
+
+    public TransactionLogController (){
+
+    }
+
+}
